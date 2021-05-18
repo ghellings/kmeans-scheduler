5f444102b4a4c12d942f2c637bc223ad48820aab
Greg Hellings <greg@searchspring.com>
2021-05-18 15:47:51 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/muesli/clusters"
+	"github.com/muesli/kmeans"
+)
+
+func main() {
+
+	sitemap, d := parsecsv("./data.csv")
+
+	// Partition the data points into 16 clusters
+	km := kmeans.New()
+	clusters, err := km.Partition(d, 3)
+	if err != nil {
+		panic(fmt.Sprintf("%v", err))
+	}
+	for i, c := range clusters {
+
+		fmt.Printf("Cluster %v\n", i)
+		for _, o := range c.Observations {
+			statmap := makestatmap(o.Coordinates())
+			for _, site := range sitemap[statmap] {
+				fmt.Printf("%v\n", site)
+			}
+		}
+
+	}
+}
+
+func parsecsv(file string) (sitemap map[string][]string, d clusters.Observations) {
+	sitemap = make(map[string][]string)
+	csvfile, err := os.Open(file)
+	if err != nil {
+		panic(fmt.Sprintf("%v", err))
+	}
+	r := csv.NewReader(csvfile)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		site := record[0]
+		record = record[1:]
+		stats := []float64{}
+		for _, val := range record {
+			fval, err := strconv.ParseFloat(val, 64)
+			if err != nil {
+				panic(fmt.Sprintf("Can't convert %v %v", val, err))
+			}
+			stats = append(stats, fval)
+		}
+		d = append(d, clusters.Coordinates{
+			stats[0],
+			stats[1],
+			stats[2],
+			stats[3],
+			stats[4],
+		})
+		statmap := makestatmap(stats)
+		sitemap[statmap] = append(sitemap[statmap], site)
+	}
+	return
+}
+
+func makestatmap(stats []float64) (statmap string) {
+	for _, val := range stats {
+		if statmap == "" {
+			statmap = fmt.Sprintf("%f", val)
+		} else {
+			statmap = fmt.Sprintf("%s-%f", statmap, val)
+		}
+	}
+	return
+}
